b98768453670515818994e1bba843f96a4af5502
HrishiAwari <hrishiawari007@gmail.com>
2017-08-15 17:40:02 +0530

    Align

--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    this.User="Hrishi"
+  }
+User:String;
 
   ngOnInit() {
   }
